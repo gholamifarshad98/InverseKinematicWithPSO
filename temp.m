a61e3e85963ff40b8cb7135fabb282920d9ccd84
Farshad Ghloami <gholamifarshad98@yahoo.com>
2022-08-08 22:51:34 +0430

    now the aera is added to the robot path.

--- temp.m
+++ temp.m
@@ -6,6 +6,16 @@ close all;
 L1 = Link('d', 0, 'a', 0.5, 'alpha', 0);
 L2 = Link('d', 0, 'a', 0.5, 'alpha', 0);
 bot = SerialLink([L1 L2], 'name', 'my robot');
+eeps = []
+i= 0 
+j = 0
+for theta1= 0: 0.1:2*pi
+    for theta2 = 0:0.1:2*pi
+        j = j+1
+        eeps(j,:)= [bot.fkine([theta1 theta2]).t(1) bot.fkine([theta1 theta2]).t(2)];
+        
+    end    
+end
 
 %%
 LoadedDataEF = load('XYPathEF.mat');
@@ -21,9 +31,16 @@ theta1_vec = storedResults(1:20,1)
 theta2_vec = storedResults(1:20,2)
 
 figure(2)
-plot(x_desier_vec(1:20),y_desier_vec(1:20))
+plot(x_desier_vec(1:20),y_desier_vec(1:20),'m','LineWidth',3)
 hold on
-bot.plot(storedResults(1:20,:))
+plot(eeps(:,1),eeps(:,2))
+
+anim = Animate('movie.mp4');
+for i=1:20
+bot.plot(storedResults(i,:))
+anim.add();
+end
+anim.close();
 
 figure(12)
 subplot(2,1,1);
@@ -35,7 +52,7 @@ plot(time_vec,theta2_vec)
 ylabel('\theta_2 (rad)')
 xlabel('Time (s)')
 figure(10)
-plot(x_desier_vec(1:20),y_desier_vec(1:20));
+plot(x_desier_vec(1:20),y_desier_vec(1:20),'m');
 hold on
 xlabel('X (m)')
 ylabel('Y (m)')
